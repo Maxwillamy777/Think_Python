38953b9e8b1a66e69af93cd8792d09693f539f1a
Max Willamy <166521121+Maxwillamy777@users.noreply.github.com>
2024-07-03 12:26:18 -0300

    Primeiros códigos que faço sobre o livro 'Think Python'!

--- Exercícios 1.2.py
+++ Exercícios 1.2.py
@@ -1,9 +1,9 @@
 res1 = (60 * 42 + 42)
 print(f'1. Há {res1} segundos.')
 res2 = 10 * 0.62137119
-print(f'Terá {res2} milhas.')
+print(f'2. Terá {res2} milhas.')
 res3 = 42 + 42/60
 res3b = res3 / res2
 res3c = 0.87 * 60
-res3d = 6.21371 / (47.7 / 60)
-print(f'Passo médio: {res3} minutos. A velocidade média: {res3d} milhas por horas.')
+res3d = 6.21371 / (42.7 / 60)
+print(f'3. Passo médio: {res3} minutos. A velocidade média: {res3d} milhas por horas.')
