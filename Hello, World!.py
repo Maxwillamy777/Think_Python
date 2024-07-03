5a9c9ce850e68e0c95d9980944276cfdaab97a31
Max Willamy <166521121+Maxwillamy777@users.noreply.github.com>
2024-07-03 11:33:23 -0300

    Primeiros códigos que faço sobre o livro 'Think Python'!

---
+++ Hello, World!.py
@@ -0,0 +1,14 @@
+print('Hello, World!')
+#Devemos sempre tradicionalmente  começar qualquer projeto com um olá, mundo!
+
+print(type('Hello, World!'))
+
+print("2" + "4")
+
+print('2' + '4')
+
+
+print(1,000,000)
+#Interessante!
+
+
